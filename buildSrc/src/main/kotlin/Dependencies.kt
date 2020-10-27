88c8938ea63398cc4388f2a73207ceb1cb0bbabc
marregui <miguel.arregui@gmail.com>
2020-10-27 13:02:35 +0100

    Correct attribute in reference to reflection version

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -33,7 +33,7 @@ object Versions {
 
 object Libs {
     val kotlinCoroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.kotlinCoroutines}"
-    val kotlinReflect = "org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlinCoroutines}"
+    val kotlinReflect = "org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlin}"
 
     val logging = "org.apache.logging.log4j:log4j-slf4j-impl:${Versions.log4j_slf4j}"
     val junitJupiter = "org.junit.jupiter:junit-jupiter:${Versions.junit_jupiter}"
